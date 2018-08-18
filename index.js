3d862b8c87e6300b7bae39ee93828b516f4ab66d
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-18 19:58:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+    return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  let current = document.querySelector('#grand-node')
+  let arr = []
+  while(current){
+      arr.push(current);
+  	  current = current.children[0]
+  }
+  return arr[arr.length-1]
+}
